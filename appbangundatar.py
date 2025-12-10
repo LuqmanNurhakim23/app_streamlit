b048a07766f6f3af9ba4ae1751ed9d2c762df677
Luqman Nurhakim <uchluqman146@gmail.com>
2025-12-10 19:55:24 +0700

    selesai

--- appbangundatar.py
+++ appbangundatar.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title ('aplikasi perhitungan luas bangun datar')
 st.header ('ini buatan anak SI')
 
-menu = st.sidebar.selectbox('pilih bangun datar', ['luas persegi','luas segitiga','luas segitiga','luas lingkaran'])
+menu = st.sidebar.selectbox('pilih bangun datar', ['luas persegi','luas segitiga','luas lingkaran'])
 
 if menu == 'luas persegi':
     st.write ('ini halaman untuk menghitung luas persegi')
@@ -16,3 +16,17 @@ if menu == 'luas persegi':
 
 elif menu == 'luas segitiga':
     st.write ('ini halaman untuk menghitung luas segitiga')
+    st.image ('https://imgs.search.brave.com/5KJsqP7W_WAIVBe5O16A1RpWJekxh9zwajS45AjyNuM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZS5pZG50aW1lcy5j/b20vcG9zdC8yMDIy/MDMzMS9zZWdpdGln/YS1iZWRiNmJmYTgx/ZjM1M2UxZDAwNjE5/YzE2NTdlNmJkYy5w/bmc', caption ='gambar segitiga')
+    alas = st.number_input('silakan masukan alas', min_value=0)
+    tinggi = st.number_input('silakan masukan tinggi', min_value=0)
+    if st.button('hitung'):
+        luas = 0.5 * alas * tinggi
+        st.success(f'luas segitiga adalah {luas}')
+
+elif menu == 'luas lingkaran':
+    st.write ('ini halaman untuk menghitung luas lingkaran')
+    st.image ('https://imgs.search.brave.com/3pyGqiuLONBl6JHZ8iKgamH7EFYUqYloW_1vF9U9cNI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9naG9z/dC1ibG9nLWFzc2V0/cy56ZW5pdXMubmV0/L3dvcmRwcmVzcy8y/MDIxLzA5L2xpbmdr/YXJhbi5wbmc', caption ='gambar lingkaran')
+    jarijari = st.number_input('silakan masukan jari jari', min_value=0)
+    if st.button('hitung'):
+        luas = 3.14 * (jarijari **2)
+        st.success(f'luas lingkaran adalah adalah {luas}')
